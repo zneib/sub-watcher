df8bb11bdac67f040522b38e3b3656153137a727
Zach Neibaur <zneibaur@protonmail.com>
2022-06-28 00:20:50 -0600

    Setting up some more of the timer functionality

--- src/Player.svelte
+++ src/Player.svelte
@@ -2,19 +2,30 @@
   export let name;
   export let removeActivePlayer;
   let count = 0;
+  let minutes = 0
 
   setInterval(() => {
-    count += 1;
+    if (count === 59) {
+      minutes += 1;
+      count = 0;
+    } else {
+      count += 1;
+    }
   }, 1000);
+
+  $: timer = `${minutes < 10 ? `0${minutes}` : minutes}:${count < 10 ? `0${count}` : count}`;
 </script>
 
 <div on:click={removeActivePlayer(name)}>
-  <span>{count}</span>
   {name}
+  <span>{timer}</span>
 </div>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap');
   div {
+    display: flex;
+    justify-content: space-between;
     background: #fff;
     border-radius: 5px;
     box-shadow: 0px 0px 0px 0px rgba(0,0,0,0.2);
@@ -25,5 +36,9 @@
   div:hover {
     box-shadow: 0px 2px 6px 2px rgba(0,0,0,0.2);
     cursor: pointer;
+  }
+
+  span {
+    font-family: 'Allerta Stencil', sans-serif;
   }
 </style>
